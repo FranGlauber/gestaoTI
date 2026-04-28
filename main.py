3b9eb77b443a9cc2d76066d8ec5b283108e62abb
FranGlauber <fglauber912@gmail.com>
2026-04-27 22:11:40 -0300

    enviar primeiros arquivos do projeto inicial

---
+++ main.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+@app.route('/index')
+def home():
+    return render_template('index.html')
+
+@app.route('/usuarios')
+def usuarios():
+    return render_template('usuarios.html')
+
+@app.route('/produtos')
+def produtos():
+    return render_template('produtos.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
